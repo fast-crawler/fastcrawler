df06aa204e8551acbd2fdba85d4d45107a4e4321
Mani <mani.mozaffar@gmail.com>
2023-06-11 18:02:36 +0300

    added: typelint and docs to engine proto

--- fastcrawler/engine/playwright.py
+++ fastcrawler/engine/playwright.py
@@ -1,18 +1,40 @@
-import typing
+from typing import List
 
 import pydantic
-from playwright.async_api import ProxySettings, async_playwright
+from playwright.async_api import Browser, Page, ProxySettings, async_playwright
+from playwright.async_api._context_manager import PlaywrightContextManager
 
-from fastcrawler.engine.proto import EngineProto, ProxySetting
+from fastcrawler.engine.proto import EngineProto, ProxySetting, SetCookieParam
 
 
 class Playwright(EngineProto):
+    """
+    Playwrright is one of the engines written from the protocol, using playwright library.
+
+    To use this engine in library, simply set as your Spider's class's attribute and you're good to go:)
+    Example:
+
+    class MySpider(Spider):
+        engine = engine.Playwright
+
+
+    If you wish to change the behavior of this engine, you can create your own engine adhering from
+        this engine, simply subclass this class and override methods you want to change.
+
+    By default, it uses `Mozila Firefox` browser which I assume is the best one out there,
+        no CDP and etc, less overhead.
+
+    """
+    page: Page
+    browser: Browser
+    async_manager: PlaywrightContextManager | None
 
     def __init__(
         self,
         headless: bool = True,
-        proxy: ProxySetting = None,
-        cookies: typing.List[dict] = None,
+        proxy: ProxySetting | None = None,
+        cookies: List[SetCookieParam] | None = None,
+        useragent: dict | None = None
     ):
         """ initilize the playwright with proxy, headless, and cookies
         """
@@ -26,6 +48,9 @@ class Playwright(EngineProto):
             )
         self.headless = headless
         self.cookies = cookies
+        self.useragent = useragent
+        self.driver = None
+        self.context = None
 
     async def setup(self) -> None:
         """
@@ -41,50 +66,54 @@ class Playwright(EngineProto):
             proxy=self._proxy,
         )
         if self.cookies:
-            await self.context.add_cookies(self.cookies)
+            await self.context.add_cookies(self.cookies)  # type: ignore
         self.page = await self.context.new_page()
         await self.page.bring_to_front()
         return None
 
-    async def base(self, url: pydantic.AnyUrl, method) -> str:
+    async def base(self, url: pydantic.AnyUrl, method: str, data: dict | None = None) -> str:
         """
         Base method to execute different HTTP methods for crawling purpose
         """
-        await getattr(self.page, method)(url)
-        return await self.page.content()
+        if not data:
+            await getattr(self.page, method)(url)
+        else:
+            raise NotImplementedError("Passing body data is not supported in browser protocol")
+        return await self.page.content()  # type: ignore
 
-    async def get(self, urls: typing.List[pydantic.AnyUrl]):
+    async def get(self, urls: List[pydantic.AnyUrl]) -> list:
         """
         Although Playwright is async, but URL must be retrieved in sync if one browser is being used
         """
         results = []
         for url in urls:
-            result = await self.base(url, "goto")
+            result = await self.base(url, "goto", data=None)
             results.append(result)
         return results
 
-    async def post(self):
+    async def post(self, urls: List[pydantic.AnyUrl] = ..., datas: List[dict] = ...):  # type: ignore
         raise NotImplementedError(
-            "This method is not implemented yet for playwright engine."
+            "POST method is not implemented yet for playwright engine."
         )
 
-    async def put(self):
+    async def put(self, urls: List[pydantic.AnyUrl] = ..., datas: List[dict] = ...):  # type: ignore
         raise NotImplementedError(
-            "This method is not implemented yet for playwright engine."
+            "PUT method is not implemented yet for playwright engine."
         )
 
-    async def delete(self):
+    async def delete(self, urls: List[pydantic.AnyUrl] = ..., datas: List[dict] = ...):  # type: ignore
         raise NotImplementedError(
-            "This method is not implemented yet for playwright engine."
+            "GET method is not implemented yet for playwright engine."
         )
 
     async def teardown(self) -> None:
         """
         close the playwright browser and async manager
         """
-        await self.browser.close()
-        await self.async_manager.__aexit__()
-        self.async_manager = None
+        if isinstance(self.async_manager, PlaywrightContextManager):
+            await self.browser.close()
+            await self.async_manager.__aexit__()
+            self.async_manager = None
         return None
 
     async def __aenter__(self):
