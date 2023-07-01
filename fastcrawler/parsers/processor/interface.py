b0d282c73b922bfcbdcbeb74f6520eb37d9e1be4
ManiMozaffar <mani.mozaffar@gmail.com>
2023-07-01 07:54:18 +0300

    added: docs/type hints

--- fastcrawler/parsers/processor/interface.py
+++ fastcrawler/parsers/processor/interface.py
@@ -1,21 +1,50 @@
-from typing import Protocol
+from typing import List, Protocol
 
 
 class ElementInterface(Protocol):
-    def get(self, key, default=None): ...
+    def get(self, key: str, default=None):
+        """
+        get method, which resolves an HTML element from a given key
+            for instance:
+
+            <a href=...>
+            get(href) -> returns the href text inside the given element
+                can also have default if nothing is found. Should be None.
+        """
 
     @property
-    def text(self): ...
+    def text(self):
+        """
+        Gets the inner text from an HTML element
+
+            for instance:
+                <li>Hello Mani :)</li>
+                element.text -> 'Hello Mani'
+        """
 
 
 class ProcessorInterface(Protocol):
     base_element: ElementInterface = ...
 
     @staticmethod
-    def to_string(result: ElementInterface): ...
+    def to_string(result: ElementInterface) -> str:
+        """
+        Resolves a result to string, by getting the inner html,
+        This method is used to iterate over HTML elements to resolve inner pydantic models
+        """
 
     @staticmethod
-    def from_string_by_xpath(string: str, query: str) -> ElementInterface: ...
+    def from_string_by_xpath(
+        string: str, query: str
+    ) -> List[ElementInterface] | ElementInterface | None:
+        """
+        Resolves a HTML string by XPATH
+        """
 
     @staticmethod
-    def from_string_by_css(string: str, query: str) -> ElementInterface: ...
+    def from_string_by_css(
+        string: str, query: str
+    ) -> List[ElementInterface] | ElementInterface | None:
+        """
+        Resolves a HTML string by CSS
+        """
